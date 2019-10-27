e2812f324c3d0d65a77b0a2c3c0efdc61f395bc9
bastiion <graviola@posteo.de>
2019-10-27 19:16:23 +0100

    alter route calculation based on diffrent remotes

--- src/main/app/ui/leaflet/state.cljs
+++ src/main/app/ui/leaflet/state.cljs
@@ -10,41 +10,51 @@
   "For now we don't require arguments, but always reload all datasets"
   [_]
   (action [{:keys [app]}]
-    (doseq [[ds-name ds] (:leaflet/datasets (current-state SPA))]
-           (let [source (:source ds)
-                 [ident params] (if (keyword? (:query source))
-                                    [(:query source) nil]
-                                    [:_ {:query (:query source)}])]
-                (load! app ident nil {:remote (:remote source)
-                                      :params params
-                                      :target [:leaflet/datasets ds-name :data :geojson]})))))
+          (doseq [[ds-name ds] (:leaflet/datasets (current-state SPA))]
+            (let [source (:source ds)
+                  [ident params] (if (keyword? (:query source))
+                                   [(:query source) nil]
+                                   [:_ {:query (:query source)}])]
+              (load! app ident nil {:remote (:remote source)
+                                    :params params
+                                    :target [:leaflet/datasets ds-name :data :geojson]})))))
 
 (defmutation mutate-datasets [{:keys [path data]}]
   (action [{:keys [app state]}]
-    (swap! state update-in (concat [:leaflet/datasets] path)
-                           (fn [d_orig d_new] (if (map? d_new) (merge d_orig d_new) d_new))
-                           data)
-    (transact! app [(mutate-datasets-load)])))
+          (swap! state update-in (concat [:leaflet/datasets] path)
+                 (fn [d_orig d_new] (if (map? d_new) (merge d_orig d_new) d_new))
+                 data)
+          (transact! app [(mutate-datasets-load)])))
 
 (defmutation mutate-layers [{:keys [path data]}]
   (action [{:keys [state]}]
-    (swap! state update-in (concat [:leaflet/layers] path)
-                           (fn [d_orig d_new] (if (map? d_new) (merge d_orig d_new) d_new))
-                           data)))
+          (swap! state update-in (concat [:leaflet/layers] path)
+                 (fn [d_orig d_new] (if (map? d_new) (merge d_orig d_new) d_new))
+                 data)))
+
+(def graphhopper-remote #_:graphhopper :graphhopper-web)
+
+(defn load-route
+  [fromPoint toPoint app]
+  (let [request {:remote graphhopper-remote
+                 :params {
+                          :start fromPoint
+                          :end   toPoint
+                          }
+                 :target [:leaflet/datasets :routing :data]
+                 }]
+    (prn "Will request")
+    (prn request)
+    (load! app :graphhopper/route nil request))
+
+  )
 
 (defmutation current-point-select [props]
   (action [{:keys [app state]}]
           (let [points (:selected/points @state)]
             (if (> 1 (count points))
               nil
-              (let [request {:remote :graphhopper-web
-                             :params {
-                                      :start (:selected/latlng (last points))
-                                      :end    props
-                                      }
-                             :target [:leaflet/datasets :routing :data]
-                             }]
-                (load! app :graphhopper/route nil request))
+              (load-route (:selected/latlng (last points)) props app)
               )
             (swap! state into {:selected/points (vec (conj (:selected/points @state) {:selected/latlng props}))})
 
